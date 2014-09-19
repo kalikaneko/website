80be8a9a09865e91ff36d37ae3bfa0450d5ca286
joates <github@joat.es>
2014-09-19 03:07:50 +0100

    added SMTP transport module

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-var server = require('http').createServer(handler)
+var nodemailer = require('nodemailer')
+  , transporter = nodemailer.createTransport()
+  , server = require('http').createServer(handler)
   , fs = require('fs')
   , re = new RegExp('\.js$', 'i')
   , port = process.env.PORT || /*80*/ 8000
@@ -14,8 +16,28 @@ function handler(req, res) {
 
   if (/^\/email\?/.test(req.url)) {
     var params = require('url').parse(req.url, true)
-    if (params && params.query.email)   // @TODO: validate input
+    if (params && params.query.email) {
+
+      /*
+      var opts = {
+        from: 'news-mailer@squatconf.eu',
+        to: params.query.email,
+        subject: "Hello, everyone is welcome at SquatConf..",
+        text: 'Please verify that you wish to signup by following this link\n'
+            + '[##> link here <##]\n\n'
+            + 'You can ignore this message if you DID NOT request to signup at our website\n'
+            + 'http://squatconf.eu\n\n'
+            + 'next event is in Paris, we hope to see you there !!\n'
+            + 'kind regards from the team,\nSquatConf Paris 2014'
+      }
+      transporter.sendMail(opts, function(err, data) {
+        if (err) return console.error('email problem !', err)
+        console.log('email sent', data)
+      })
+      */
+
       console.log('got email:', params.query)
+    }
     res.statusCode = 302
     res.setHeader('Location', '/')
     return res.end()
